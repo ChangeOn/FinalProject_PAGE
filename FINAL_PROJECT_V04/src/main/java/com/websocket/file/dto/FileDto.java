c47a26d8fe315415f9e50395b794db15d706575d
rezacal <perln@naver.com>
2019-03-25 16:12:33 +0900

    fix websocket Dto

--- FINAL_PROJECT_V04/src/main/java/com/websocket/file/dto/FileDto.java
+++ FINAL_PROJECT_V04/src/main/java/com/websocket/file/dto/FileDto.java
@@ -1,18 +1,43 @@
 package com.websocket.file.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Setter
-@Getter
-@AllArgsConstructor
-@NoArgsConstructor
 public class FileDto {
 	
 	private int fileno;
 	private String filename;
 	private String filesavename;
+	
+	public FileDto(int fileno, String filename, String filesavename) {
+		super();
+		this.fileno = fileno;
+		this.filename = filename;
+		this.filesavename = filesavename;
+	}
+	public FileDto() {
+		super();
+	}
+	
+	public int getFileno() {
+		return fileno;
+	}
+	public void setFileno(int fileno) {
+		this.fileno = fileno;
+	}
+	public String getFilename() {
+		return filename;
+	}
+	public void setFilename(String filename) {
+		this.filename = filename;
+	}
+	public String getFilesavename() {
+		return filesavename;
+	}
+	public void setFilesavename(String filesavename) {
+		this.filesavename = filesavename;
+	}
+	
+	
+	
+	
+	
 
 }
